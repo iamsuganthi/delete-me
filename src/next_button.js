a76f5c501876aa66c69e4bbfc2e91c9813047204
Suganthi Krishnavathi <suganthikrishnavathi@MacBook-Pro.local>
2024-09-30 14:59:03 +1000

    Revert "Revert "Revert "add code"""
    This reverts commit 64bbde1ae847cff0f9251093f87bc0e762f6b224.

--- src/next_button.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-class NextButton extends React.Component {
-  render() {
-    return <button className="nextButton" onClick={this.props.nextSection}>Next</button>;
-  }
-}
-
-module.exports = NextButton;
